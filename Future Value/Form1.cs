05b918f4678e7134f756800ed737238f4605c18d
John Cokins <il7148pg@go.minnstate.edu>
2020-02-26 22:52:29 -0600

    exception handeling added, many type errors

--- Future Value/Form1.cs
+++ Future Value/Form1.cs
@@ -18,23 +18,72 @@ namespace Future_Value
         }
 
         private void btnCalculate_Click(object sender, EventArgs e)
-        {       //converts user input to decimal numbers, years to int
-            decimal monthlyInvestment =
-                Convert.ToDecimal(txtMonthlyInvestment.Text);
-            decimal yearlyInterestRate =
-                Convert.ToDecimal(txtInterestRate.Text);
-            int years = Convert.ToInt32(txtYears.Text);
+        {
 
-            int months = years * 12;
-            decimal monthlyInterestRate = yearlyInterestRate / 12 / 100;
-            //calculates the monthly interest rate, as in this scenerio the interest is calculated
-            //every month, previous month's gains also gain interest
-            decimal futureValue = this.CalculateFutureValue(monthlyInvestment,
-                monthlyInterestRate, months);
-           
-            txtFurureValue.Text = futureValue.ToString("c");
-            txtMonthlyInvestment.Focus();
+            if (DataCheck(txtMonthlyInvestment.Text, txtInterestRate.Text, txtYears.Text))
+            {
+                if (DataTypeCheck(txtMonthlyInvestment.Text, txtInterestRate.Text,
+           txtYears, out decimal monthlyInvestment, out decimal yearlyInterestRate,
+          out int years))
+                { 
+                    int months = years * 12;
+                decimal monthlyInterestRate = yearlyInterestRate / 12 / 100;
+                //calculates the monthly interest rate, as in this scenerio the interest is calculated
+                //every month, previous month's gains also gain interest
+                decimal futureValue = this.CalculateFutureValue(monthlyInvestment,
+                    monthlyInterestRate, months);
 
+                txtFurureValue.Text = futureValue.ToString("c");
+                txtMonthlyInvestment.Focus();
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Please enter a value in each box", "Error");
+            }
+        }
+        private bool DataCheck(string txtMonthlyInvestment, string txtInterestRate,
+            string txtYears)
+        {   //checks if data is entered
+            if (String.IsNullOrEmpty(txtMonthlyInvestment))
+            {
+                return false;
+            }
+            if (String.IsNullOrEmpty(txtMonthlyInvestment))
+            {
+                return false;
+            }
+            if (String.IsNullOrEmpty(txtYears))
+            {
+                return false;
+            }
+            return true;
+        }
+        private bool DataTypeCheck(string txtMonthlyInvestment, string txtInterestRate,
+         string txtYears, out decimal monthlyInvestment, decimal yearlyInterestRate, 
+         int months)
+        {   //checks if valid data is entered
+            try
+            {
+                monthlyInvestment =
+                    Convert.ToDecimal(txtMonthlyInvestment.Text);
+                yearlyInterestRate =
+                    Convert.ToDecimal(txtInterestRate.Text);
+                years = Convert.ToInt32(txtYears.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please check the data entered, use only numbers", "Error");
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("An overflow exception has occured. Please use a smaller number", "Error");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+            }
         }
 
         private decimal CalculateFutureValue(decimal monthlyInvestment,
